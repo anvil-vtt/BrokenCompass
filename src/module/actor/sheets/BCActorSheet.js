0752c3f529d55405b3d2ee3c02eec2caf1de28ce
Hauke Frahmann <versand@frahmann.de>
2021-06-06 18:36:43 +0200

    erste Änderungen

--- src/module/actor/sheets/BCActorSheet.js
+++ src/module/actor/sheets/BCActorSheet.js
@@ -8,48 +8,48 @@ export class BCActorSheet extends ActorSheet {
     });
   }
 
-  activateListeners(html) {
-    super.activateListeners(html);
-
-    html.on("click", "[data-roll]", this._rollTheDice.bind(this));
-  }
-
-  getData(options) {
-    const data = super.getData(options);
-
-    data.fields = [
-      {
-        name: game.i18n.localize("BC.Sheet.Fields.Action"),
-        value: 3,
-        skills: [
-          {
-            name: "Fight",
-            value: 1,
-          },
-          {
-            name: "Leadership",
-            value: 3,
-          },
-          {
-            name: "Stunt",
-            value: 2,
-          },
-        ],
-      },
-      {
-        name: game.i18n.localize("BC.Sheet.Fields.Guts"),
-        skills: [],
-      },
-      {
-        name: game.i18n.localize("BC.Sheet.Fields.Knowledge"),
-        skills: [],
-      },
-    ];
-
-    data.numbers = [...Array(10).keys()];
-
-    return data;
-  }
+  // activateListeners(html) {
+  //   super.activateListeners(html);
+  //
+  //   html.on("click", "[data-roll]", this._rollTheDice.bind(this));
+  // }
+  //
+  // getData(options) {
+  //   const data = super.getData(options);
+  //
+  //   data.fields = [
+  //     {
+  //       name: game.i18n.localize("BC.Sheet.Fields.Action"),
+  //       value: 3,
+  //       skills: [
+  //         {
+  //           name: "Fight",
+  //           value: 1,
+  //         },
+  //         {
+  //           name: "Leadership",
+  //           value: 3,
+  //         },
+  //         {
+  //           name: "Stunt",
+  //           value: 2,
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       name: game.i18n.localize("BC.Sheet.Fields.Guts"),
+  //       skills: [],
+  //     },
+  //     {
+  //       name: game.i18n.localize("BC.Sheet.Fields.Knowledge"),
+  //       skills: [],
+  //     },
+  //   ];
+  //
+  //   data.numbers = [...Array(10).keys()];
+  //
+  //   return data;
+  // }
 
   async _rollTheDice(e) {
     const rollString = e.currentTarget.dataset.roll;
